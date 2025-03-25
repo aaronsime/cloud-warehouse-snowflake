bdd4f4279cb208f6780f855e06d4b52da007dfe1
aaron <aaronsime@hotmail.co.nz>
2025-03-25 17:27:23 +1300

    adding resources to stage files from gcs

--- terraform/staging.tf
+++ terraform/staging.tf
@@ -8,14 +8,15 @@ resource "snowflake_file_format" "csv_format" {
 }
 
 resource "snowflake_stage" "raw_stage" {
-  depends_on = [snowflake_grant_privileges_to_account_role.file_format_usage]
   name      = "RAW_STAGE"
   database  = var.database
   schema    = "RAW"
 
-  url                 = var.gcp_bucket
+  url                 = "gcs://outbound-snowflake-dev/raw/incoming"
   storage_integration = "GCS_INT"
-  file_format         = "CSV_FORMAT"
+
+  file_format = "FORMAT_NAME = DEV_CLOUD_DATAWAREHOUSE.RAW.CSV_FORMAT"
 
   comment = "Stage for raw olist incoming data"
 }
+
