cd6bb42ecb60ff20f8571c06b78bc7d5a2c37ece
aaron <aaronsime@hotmail.co.nz>
2025-03-28 16:41:39 +1300

    adding gcp user config

--- terraform/users.tf
+++ terraform/users.tf
@@ -1,27 +1,33 @@
+resource "snowflake_account_role" "gcp_user_role" {
+  name = "GCP_USER_ROLE"
+}
+
 resource "snowflake_user" "gcp_user" {
-  name         = "GCP_USER"
-  password     = var.gcp_snowflake_password
-  login_name   = "GCP_USER"
-  default_role = snowflake_role.gcp_loader_role.name
+  name              = "GCP_USER"
+  login_name        = "GCP_USER"
+  password          = var.gcp_snowflake_password
+  default_role      = snowflake_account_role.gcp_user_role.name
   default_warehouse = var.warehouse
   must_change_password = false
 }
 
-resource "snowflake_role" "gcp_loader_role" {
-  name = "GCP_LOADER_ROLE"
+resource "snowflake_grant_account_role" "gcp_user_grant" {
+  role_name = snowflake_account_role.gcp_user_role.name
+  user_name = snowflake_user.gcp_user.name
 }
 
-resource "snowflake_grant_privileges_to_account_role" "gcp_loader_role_grants" {
-  account_role_name = snowflake_role.gcp_loader_role.name
+resource "snowflake_grant_privileges_to_account_role" "gcp_user_schema_grants" {
+  account_role_name = snowflake_account_role.gcp_user_role.name
 
   on_schema {
     all_schemas_in_database = var.database
   }
 
-  privileges = ["USAGE", "CREATE STAGE", "CREATE FILE FORMAT", "CREATE TABLE", "INSERT"]
-}
-
-resource "snowflake_role_grants" "grant_loader_user_role" {
-  role_name = snowflake_role.gcp_loader_role.name
-  users     = [snowflake_user.gcp_user.name]
+  privileges = [
+    "USAGE",
+    "CREATE STAGE",
+    "CREATE FILE FORMAT",
+    "CREATE TABLE",
+    "INSERT"
+  ]
 }
